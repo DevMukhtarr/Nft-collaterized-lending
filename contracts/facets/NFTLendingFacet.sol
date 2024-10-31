a386849a838d3ae1fa1de7346ebb70b18ee385f7
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-10-31 22:09:36 +0100

    added space

--- contracts/facets/NFTLendingFacet.sol
+++ contracts/facets/NFTLendingFacet.sol
@@ -5,8 +5,7 @@ pragma solidity 0.8.4;
 import {LibDiamond} from "../libraries/LibDiamond.sol";
 import {Events, Errors} from "../libraries/Utils.sol";
 
+
 contract NFTLendFacet is ERC721Facet {
-    constructor() {
-        
-    }
+    
 }
