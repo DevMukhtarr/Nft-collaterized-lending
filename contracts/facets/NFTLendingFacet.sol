8c82cea00a208f59534939a8e018962539dbd214
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-10-31 22:05:26 +0100

    initial commit

---
+++ contracts/facets/NFTLendingFacet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+import {Events, Errors} from "../libraries/Utils.sol";
+
+contract NFTLendFacet is ERC721Facet {
+    constructor() {
+        
+    }
+}
